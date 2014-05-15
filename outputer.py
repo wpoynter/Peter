8992b0756108d55fbba00cb2ef340054d9cf18c3
wpoynter <poynter.william@gmail.com>
2014-05-15 10:37:01 +0100

    Question constructs now use the variable name instead of ID

--- outputer.py
+++ outputer.py
@@ -92,9 +92,9 @@ class Outputer(object):
 				respUnitSqlID = respUnits[0].sqlID
 			self.ids['cc_questions'] += 1
                         f.write('INSERT INTO cc_questions (id, textid, question_reference_id, response_unit_id, created_at, updated_at, question_reference_type) VALUES ')
-                        f.write('(' + str(self.ids['cc_questions'])  + ',"qc_' + str(question.ID) + '",' + str(question.sqlID)  + ',' + str(respUnitSqlID) + ',"' + str(question.created_at) + '","' + str(question.updated_at) + '","QuestionItem")')
+                        f.write('(' + str(self.ids['cc_questions'])  + ',"qc_' + str(question.name) + '",' + str(question.sqlID)  + ',' + str(respUnitSqlID) + ',"' + str(question.created_at) + '","' + str(question.updated_at) + '","QuestionItem")')
 			f.write(';\n')
-			self.question_constructs.append(QuestionConstruct('qc_' + str(question.ID), question.sqlID, respUnitSqlID))
+                        self.question_constructs.append(QuestionConstruct('qc_' + str(question.name), question.sqlID, respUnitSqlID))
 			self.question_constructs[-1].sqlID = self.ids['cc_questions']
 			for code_scheme in self.data['code_schemes']:
 				if code_scheme.ID == question.code_scheme_id:
